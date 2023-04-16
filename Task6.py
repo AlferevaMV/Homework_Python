43476730f1f9545e3bc702acc3f5f754179f63e0
alferevamarina <alferevamv@mail.ru>
2023-04-17 00:04:35 +0300

    Task6

--- Task6.py
+++ Task6.py
@@ -5,3 +5,15 @@
 
 # 385916 -> yes
 # 123456 -> no
+
+
+n = str(input())
+
+sum1=int(n[0])+int(n[1])+int(n[2])
+sum2=int(n[3])+int(n[4])+int(n[5])
+if sum1==sum2:
+  print('Да, счастливый!')
+else:
+  print('Нет, обычный!')
+
+
